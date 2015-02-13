29880390f0a6ac788ac1d1577a289265019bc2f2
tiktok12 <tiktok12@users.noreply.github.com>
2015-02-13 10:11:27 +0200

    url links  would not exit from laravel.com
    add the posiblity to open website without exit from laravel.com
    
    only laravel , forge and laracasts will open on the same window as a defualt.

--- resources/views/partials/main-nav.blade.php
+++ resources/views/partials/main-nav.blade.php
@@ -5,21 +5,21 @@
 <li class="dropdown community-dropdown">
 	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Conference <span class="caret"></span></a>
 	<ul class="dropdown-menu" role="menu">
-		<li><a href="http://laracon.us">Laracon US</a></li>
-		<li><a href="http://laracon.eu">Laracon EU</a></li>
+		<li><a href="http://laracon.us" target="_blank">Laracon US</a></li>
+		<li><a href="http://laracon.eu" target="_blank">Laracon EU</a></li>
 	</ul>
 </li>
 <li class="dropdown community-dropdown">
 	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Community <span class="caret"></span></a>
 	<ul class="dropdown-menu" role="menu">
-		<li><a href="https://github.com/laravel/laravel">Github</a></li>
-		<li><a href="https://twitter.com/laravelphp">Twitter</a></li>
+		<li><a href="https://github.com/laravel/laravel" target="_blank">Github</a></li>
+		<li><a href="https://twitter.com/laravelphp" target="_blank">Twitter</a></li>
 		<li class="divider"></li>
-		<li><a href="https://laracasts.com/discuss">Laracasts Forums</a></li>
-		<li><a href="http://laravel.io/forum">Laravel.io Forums</a></li>
-		<li><a href="http://larachat.co">LaraChat Slack Channel</a></li>
-		<li><a href="https://laravel-news.com/">Laravel News</a></li>
+		<li><a href="https://laracasts.com/discuss" target="_blank">Laracasts Forums</a></li>
+		<li><a href="http://laravel.io/forum" target="_blank">Laravel.io Forums</a></li>
+		<li><a href="http://larachat.co" target="_blank">LaraChat Slack Channel</a></li>
+		<li><a href="https://laravel-news.com/" target="_blank">Laravel News</a></li>
 		<li class="divider"></li>
-		<li><a href="https://larajobs.com/?partner=5#/">LaraJobs</a></li>
+		<li><a href="https://larajobs.com/?partner=5#/" target="_blank">LaraJobs</a></li>
 	</ul>
 </li>
